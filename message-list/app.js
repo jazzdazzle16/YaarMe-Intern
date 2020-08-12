0e7e7b35034ee126e6942d58b7deda8ef91cbb91
jess1601 <69599366+jess1601@users.noreply.github.com>
2020-08-12 23:58:57 +0530

    Add files via upload

---
+++ message-list/app.js
@@ -0,0 +1,12 @@
+
+// var filbar = document.getElementById("filter");
+// var filcon = document.getElementById("filter-icon");
+
+// filcon.addEventListener("click",function(){
+//     filbar.classList.toggle("hide");
+// });
+
+document.querySelector(".filter-img").addEventListener("click",function(){
+    document.querySelector(".filter-bar").classList.toggle("hide")
+});
+
